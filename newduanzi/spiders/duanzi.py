544add92074b69e4aa129a79cfbd91acfa06d714
caiwenming <caiwenming@myhexin.com>
2019-01-12 09:06:30 +0800

    change file

--- newduanzi/spiders/duanzi.py
+++ newduanzi/spiders/duanzi.py
@@ -19,7 +19,7 @@ class DuanziSpider(CrawlSpider):
             content = earch.xpath("./div[@class='views-field-phpcode-1']/a/text()").extract()[0]
             source = earch.xpath("./div[@class='xqjulistwafo']/a/text()").extract()[0]
             like = earch.xpath("./div[@class='views-field-ops']/a/text()").extract()[0]
-            juji = earch.xpath("./div[@class='alllistaddtoalbum']/a/text()").extract()[0]
+            juji = earch.xpath("./div[@class='views-field-field-addtoalbum-value']/a/text()").extract()[0]
             comment = earch.xpath("./div[@class='views-field-comment-count']/a/text()").extract()[0]
             author = earch.xpath("./div[@class='views-field-name']/a/text()").extract()[0]
             print ('......................%s, %s, %s, %s, %s, %s'%(content, source, like, juji, comment, author))
