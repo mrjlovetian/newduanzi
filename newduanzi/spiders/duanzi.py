34e8feeac1471af63225d97cd275ea706c783c90
caiwenming <caiwenming@myhexin.com>
2019-01-12 09:57:11 +0800

    change file

--- newduanzi/spiders/duanzi.py
+++ newduanzi/spiders/duanzi.py
@@ -16,12 +16,18 @@ class DuanziSpider(CrawlSpider):
     def parse(self, response):
         for earch in response.xpath("//div[@class='view-content']/div/div"):
             
-            content = earch.xpath("./div[@class='views-field-phpcode-1']/a/text()").extract()[0]
-            # source = earch.xpath("./div[@class='xqjulistwafo']/a/text()").extract()[0]
-            like = earch.xpath("./div[@class='views-field-ops']/a/text()").extract()[0]
-            juji = earch.xpath("./div[@class='views-field-field-addtoalbum-value']/div/a/text()").extract()[0]
-            comment = earch.xpath("./div[@class='views-field-comment-count']/a/text()").extract()[0]
-            author = earch.xpath("./div[@class='views-field-name']/div/a/text()").extract()[0]
+            content = earch.xpath("./div[@class='views-field-phpcode-1']/a/text()").extract()
+            content = len(content)>0?content[0]:''
+            sources = earch.xpath("./div[@class='xqjulistwafo']/a/text()").extract()
+            source = len(content)>0?sources[0]:''
+            likes = earch.xpath("./div[@class='views-field-ops']/a/text()").extract()
+            like = len(content)>0?likes[0]:''
+            jujis = earch.xpath("./div[@class='views-field-field-addtoalbum-value']/div/a/text()").extract()
+            juji = len(content)>0?jujis[0]:''
+            comments = earch.xpath("./div[@class='views-field-comment-count']/a/text()").extract()
+            comment = len(content)>0?comments[0]:''
+            authors = earch.xpath("./div[@class='views-field-name']/div/a/text()").extract()
+            author = len(content)>0?authors[0]:''
             print ('......................%s, %s, %s, %s, %s'%(content, like, juji, comment, author))
             
 
